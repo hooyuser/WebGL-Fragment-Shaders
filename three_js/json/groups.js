4b99de0cd628ef17512efdcdd14e9b32e4501f51
hooyuser <hooyuser@outlook.com>
2021-02-09 12:11:18 +0800

    Add presentation

--- three_js/json/groups.js
+++ three_js/json/groups.js
@@ -1,66 +1,356 @@
 export const nameLists = [
-    {'\u2205': ''},
-    {'\u2124\u2081': {'label': 'Z_1', 'info': ''}},
-    {'\u2124\u2082': {'label': 'Z_2', 'info': ''}},
-    {'\u2124\u2083': {'label': 'Z_3', 'info': ''}},
     {
-        '\u2124\u2084': {'label': 'Z_4', 'info': ''},
-        'D\u2084': {'label': 'D_4', 'info': ''}
-    },
-    {'\u2124\u2085': {'label': 'Z_5', 'info': ''}},
-    {
-        '\u2124\u2086': {'label': 'Z_6', 'info': ''},
-        'S\u2083': {'label': 'S_3', 'info': ''}
-    },
-    {'\u2124\u2087': {'label': 'Z_7', 'info': ''}},
-    {
-        '\u2124\u2088': {'label': 'Z_8', 'info': ''},
-        'D\u2088': {'label': 'D_8', 'info': ''},
-        'Q\u2088': {'label': 'Q_8', 'info': ''},
-        '\u2124\u2082\u00D7\u2124\u2084': {'label': 'Z_2_4', 'info': ''},
-        '\u2124\u2082\u00D7\u2124\u2082\u00D7\u2124\u2082': {'label': 'Z_2_2_2', 'info': ''}
+        "\u2205": ""
     },
     {
-        '\u2124\u2089': {'label': 'Z_9', 'info': ''},
-        '\u2124\u2083\u00D7\u2124\u2083': {'label': 'Z_3_3', 'info': ''}
+        "ℤ₁": {
+            "label": "Z_1",
+            "info": {
+                "texName": "$\\mathbb{Z}_1$",
+                "presentation": ["$\\left\\langle a \\mid a=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
     },
     {
-        '\u2124\u2081\u2080': {'label': 'Z_10', 'info': ''},
-        'D\u2081\u2080': {'label': 'D_10', 'info': ''}
+        "ℤ₂": {
+            "label": "Z_2",
+            "info": {
+                "texName": "$\\mathbb{Z}_2$",
+                "presentation": ["$\\left\\langle a \\mid a^{2}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
     },
-    {'\u2124\u2081\u2081': {'label': 'Z_11', 'info': ''}},
     {
-        '\u2124\u2081\u2082': {'label': 'Z_12', 'info': ''},
-        'D\u2081\u2082': {'label': 'D_12', 'info': ''},
-        'A\u2084': {'label': ['A_4-1','A_4-2'], 'info': ''},
-        '\u2124\u2082\u00D7\u2124\u2082\u00D7\u2124\u2083': {'label': 'Z_2_2_3', 'info': ''}
+        "ℤ₃": {
+            "label": "Z_3",
+            "info": {
+                "texName": "$\\mathbb{Z}_3$",
+                "presentation": ["$\\left\\langle a \\mid a^{3}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
     },
-    {'\u2124\u2081\u2083': {'label': 'Z_13', 'info': ''}},
     {
-        '\u2124\u2081\u2084': {'label': 'Z_14', 'info': ''},
-        'D\u2081\u2084': {'label': 'D_14', 'info': ''}
+        "ℤ₄": {
+            "label": "Z_4",
+            "info": {
+                "texName": "$\\mathbb{Z}_4$",
+                "presentation": ["$\\left\\langle a \\mid a^{4}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "D₄": {
+            "label": "D_4",
+            "info": {
+                "texName": "$D_4$",
+                "presentation": ["$\\left\\langle r, s \\mid r^{2}=s^{2}=(sr)^{2}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
     },
-    {'\u2124\u2081\u2085': {'label': 'Z_15', 'info': ''}},
     {
-        '\u2124\u2081\u2086': {'label': 'Z_16', 'info': ''},
-        'D\u2081\u2086': {'label': 'D_16', 'info': ''},
-        'Q\u2081\u2086': {'label': 'Q_16', 'info': ''},
-        '\u2124\u2082\u00D7\u2124\u2088': {'label': 'Z_2_8', 'info': ''},
-        '\u2124\u2082\u00D7\u2124\u2082\u00D7\u2124\u2084': {'label': 'Z_2_2_4', 'info': ''},
-        '\u2124\u2082\u00D7\u2124\u2082\u00D7\u2124\u2082\u00D7\u2124\u2082': {'label': 'Z_2_2_2_2', 'info': ''},
-        '\u2124\u2084\u00D7\u2124\u2084': {'label': 'Z_4_4', 'info': ''},
-        '\u2124\u2084\u00D7\u2124\u2082\u00D7\u2124\u2082': {'label': 'Z_4_2_2', 'info': ''},
+        "ℤ₅": {
+            "label": "Z_5",
+            "info": {
+                "texName": "$\\mathbb{Z}_5$",
+                "presentation": ["$\\left\\langle a \\mid a^{5}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
     },
-    {'\u2124\u2081\u2087': {'label': 'Z_17', 'info': ''}},
-    {'\u2124\u2081\u2088': {'label': 'Z_18', 'info': ''}},
-    {'\u2124\u2081\u2089': {'label': 'Z_19', 'info': ''}},
-    {'\u2124\u2082\u2080': {'label': 'Z_20', 'info': ''}},
-    {'\u2124\u2082\u2081': {'label': 'Z_21', 'info': ''}},
-    {'\u2124\u2082\u2082': {'label': 'Z_22', 'info': ''}},
-    {'\u2124\u2082\u2083': {'label': 'Z_23', 'info': ''}},
     {
-        '\u2124\u2082\u2084': {'label': 'Z_24', 'info': ''},
-        'S\u2084': {'label': 'S_4', 'info': ''}
+        "ℤ₆": {
+            "label": "Z_6",
+            "info": {
+                "texName": "$\\mathbb{Z}_6$",
+                "presentation": ["$\\left\\langle a \\mid a^{6}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "S₃": {
+            "label": "S_3",
+            "info": {
+                "texName": "$S_3$",
+                "presentation": ["$\\left\\langle r, s\\mid r^{3}=d^{2}=(sr)^{2}=e\\right\\rangle$"],
+                "iso": ["$D_6$"]
+            }
+        }
+    },
+    {
+        "ℤ₇": {
+            "label": "Z_7",
+            "info": {
+                "texName": "$\\mathbb{Z}_7$",
+                "presentation": ["$\\left\\langle a \\mid a^{7}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
+    },
+    {
+        "ℤ₈": {
+            "label": "Z_8",
+            "info": {
+                "texName": "$\\mathbb{Z}_8$",
+                "presentation": ["$\\left\\langle a \\mid a^{8}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "D₈": {
+            "label": "D_8",
+            "info": {
+                "texName": "$D_8$",
+                "presentation": ["$\\left\\langle r, s \\mid r^{4}=s^{2}=(sr)^{2}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "Q₈": {
+            "label": "Q_8",
+            "info": {
+                "texName": "$Q_8$",
+                "presentation": ["$\\left\\langle i, j \\mid i^{4}=e, i^{2}=j^{2}, ij=ji^{-1}\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "ℤ₂×ℤ₄": {
+            "label": "Z_2_4",
+            "info": {
+                "texName": "$\\mathbb{Z}_2\\times\\mathbb{Z}_4$",
+                "presentation": ["$\\left\\langle a, b \\mid a^{4}=b^{2}=e,ab=ba\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "ℤ₂×ℤ₂×ℤ₂": {
+            "label": "Z_2_2_2",
+            "info": {
+                "texName": "$\\mathbb{Z}_2\\times\\mathbb{Z}_2\\times\\mathbb{Z}_2$",
+                "presentation": ["$\\left\\langle a, b, c \\mid a^{2}=b^{2}=c^{2}=e,ab=ba,ac=bc,bc=ac\\right\\rangle$"],
+                "iso": ""
+            }
+        }
+    },
+    {
+        "ℤ₉": {
+            "label": "Z_9",
+            "info": {
+                "texName": "$\\mathbb{Z}_9$",
+                "presentation": ["$\\left\\langle a \\mid a^{9}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "ℤ₃×ℤ₃": {
+            "label": "Z_3_3",
+            "info": {
+                "texName": "$\\mathbb{Z}_3\\times\\mathbb{Z}_3$",
+                "presentation": ["$\\left\\langle a, b \\mid a^{3}=b^{3}=e,ab=ba\\right\\rangle$"],
+                "iso": ""
+            }
+        }
+    },
+    {
+        "ℤ₁₀": {
+            "label": "Z_10",
+            "info": {
+                "texName": "$\\mathbb{Z}_{10}$",
+                "presentation": ["$\\left\\langle a \\mid a^{10}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "D₁₀": {
+            "label": "D_10",
+            "info": {
+                "texName": "$D_{10}$",
+                "presentation": ["$\\left\\langle r, s \\mid r^{5}=s^{2}=(sr)^{2}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
+    },
+    {
+        "ℤ₁₁": {
+            "label": "Z_11",
+            "info": {
+                "texName": "$\\mathbb{Z}_{11}$",
+                "presentation": ["$\\left\\langle a \\mid a^{11}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        }
+    },
+    {
+        "ℤ₁₂": {
+            "label": "Z_12",
+            "info": {
+                "texName": "$\\mathbb{Z}_{12}$",
+                "presentation": ["$\\left\\langle a \\mid a^{12}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "D₁₂": {
+            "label": "D_12",
+            "info": {
+                "texName": "$D_{12}$",
+                "presentation": ["$\\left\\langle r, s \\mid r^{6}=s^{2}=(sr)^{2}=e\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "A₄": {
+            "label": [
+                "A_4-1",
+                "A_4-2"
+            ],
+            "info": {
+                "texName": "$D_{12}$",
+                "presentation": [
+                    "$\\left\\langle a, b \\mid a^{3}=b^{2}=1, aba=ba^2b\\right\\rangle$",
+                    "$\\left\\langle a, b, c \\mid a^{3}=b^{2}=c^2=1, abc=ba, bc=cb\\right\\rangle$"
+                ],
+                "iso": ""
+            }
+        },
+        "ℤ₂×ℤ₂×ℤ₃": {
+            "label": "Z_2_2_3",
+            "info": {
+                "texName": "$\\mathbb{Z}_2\\times\\mathbb{Z}_2\\times\\mathbb{Z}_3$",
+                "presentation": ["$\\left\\langle a, b, c \\mid a^{2}=b^{2}=c^{3}=e,ab=ba,ac=bc,bc=ac\\right\\rangle$"],
+                "iso": ""
+            }
+        },
+        "Q₁₂": {
+            "label": "Q_12",
+            "info": {
+                "texName": "$Q_{12}$",
+                "presentation": [""],
+                "iso": ""
+            }
+        }
+    },
+    {
+        "ℤ₁₃": {
+            "label": "Z_13",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₁₄": {
+            "label": "Z_14",
+            "info": ""
+        },
+        "D₁₄": {
+            "label": "D_14",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₁₅": {
+            "label": "Z_15",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₁₆": {
+            "label": "Z_16",
+            "info": ""
+        },
+        "D₁₆": {
+            "label": "D_16",
+            "info": ""
+        },
+        "Q₁₆": {
+            "label": "Q_16",
+            "info": ""
+        },
+        "ℤ₂×ℤ₈": {
+            "label": "Z_2_8",
+            "info": ""
+        },
+        "ℤ₂×ℤ₂×ℤ₄": {
+            "label": "Z_2_2_4",
+            "info": ""
+        },
+        "ℤ₂×ℤ₂×ℤ₂×ℤ₂": {
+            "label": "Z_2_2_2_2",
+            "info": ""
+        },
+        "ℤ₄×ℤ₄": {
+            "label": "Z_4_4",
+            "info": ""
+        },
+        "D₈×ℤ₂": {
+            "label": "D_8_Z_2",
+            "info": ""
+        },
+        "Q₈×ℤ₂": {
+            "label": "Q_8_Z_2",
+            "info": ""
+        },
+        "ℤ₄⋊ℤ₄": {
+            "label": "Z_4_s_Z_4",
+            "info": ""
+        },
+        "ℤ₄⋊D₄": {
+            "label": "Z_4_s_D_4",
+            "info": ""
+        },
+        "D₄⋊ℤ₂": {
+            "label": "D_8_s_Z_2",
+            "info": ""
+        },
+        "Q₄⋊ℤ₂": {
+            "label": "Q_8_s_Z_2",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₁₇": {
+            "label": "Z_17",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₁₈": {
+            "label": "Z_18",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₁₉": {
+            "label": "Z_19",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₂₀": {
+            "label": "Z_20",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₂₁": {
+            "label": "Z_21",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₂₂": {
+            "label": "Z_22",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₂₃": {
+            "label": "Z_23",
+            "info": ""
+        }
+    },
+    {
+        "ℤ₂₄": {
+            "label": "Z_24",
+            "info": ""
+        },
+        "S₄": {
+            "label": "S_4",
+            "info": ""
+        }
     }
 ];
 
